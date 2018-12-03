f4e1320fa6e69dbebb19949a536a466d936883d2
John Conley <john.conley@modopayments.com>
2018-12-03 14:39:43 -0600

    Added readme and inline middleware

--- src/controllers/advancedController.ts
+++ src/controllers/advancedController.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Route, Path, Query, Post, Body, Tags} from "valory-runtime";
+import {Controller, Get, Route, Path, Query, Post, Body, Tags, Middleware} from "valory-runtime";
 
 /**
  * Description can be added to alias types
@@ -22,6 +22,29 @@ export interface ModelWithEnums {
 export interface ApiRes<T> {
 	status_code: number;
 	response_data: T;
+}
+
+/**
+ * Discriminated Unions can be used to create versatile polymorphic validations
+ */
+export type Vehicle = Car | Truck | Unicycle;
+
+export interface Car {
+	// All members must have one property (discriminant) with a constant value that exactly
+	// matches their name. The discriminant can be called anything, as long as it's
+	// the same for every model in the union.
+	type: "Car";
+	all_wheel_drive: boolean;
+}
+
+export interface Truck {
+	type: "Truck";
+	wheels: 4 | 6;
+}
+
+export interface Unicycle {
+	type: "Unicycle";
+	color: PrimaryColors;
 }
 
 // You can use tags to organize groups of endpoints
@@ -50,5 +73,35 @@ export interface ApiRes<T> {
 			response_data: {potato: "cool"},
 			status_code: 1,
 		};
+	}
+
+	/**
+	 * Discriminators can be fully validated and displayed well in redoc
+	 */
+	@Post("discriminator") public discriminator(@Body() input: Vehicle) {
+		// Typescript handles discriminated unions
+
+		// Checking the discriminant allows safe access to properties
+		if (input.type === "Car") {
+			// This access is typesafe.
+			console.log(input.all_wheel_drive);
+		}
+	}
+
+	/**
+	 * The middleware decorator can be used to add inline middleware to resources
+	 */
+	@Middleware({
+		name: "InlineMiddleware",
+		handler: (request, logger, done) => {
+			done({
+				headers: {},
+				statusCode: 333,
+				body: "GO AWAY",
+			});
+		},
+	})
+	@Get("inline") public inline() {
+		// We'll never get here because the middleware forces a return
 	}
 }
